5ff423ed58ebf58981056485da60112860457587
d_sachdev <d_sachdev@us.ibm.com>
2019-09-20 09:02:41 -0400

    new change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud Pak for Applications");
+  res.send("Hello from IBM Cloud Pak for Applications, developed on Kabanero!");
 });
  
 module.exports.app = app;
