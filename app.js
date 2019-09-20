2366451840368278c117acf5c90d6dca73e555c1
d_sachdev <d_sachdev@us.ibm.com>
2019-09-20 08:42:36 -0400

    updated demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud Pak for Applications");
+  res.send("Hello from IBM Cloud Pak for Applications!");
 });
  
 module.exports.app = app;
